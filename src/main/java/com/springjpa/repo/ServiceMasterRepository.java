762fcf6d348169fa9590a1e470d009b77b5d531c
NIC <NIC@DESKTOP-3RMF15A.mshome.net>
2019-01-03 10:43:58 +0530

    ss

--- src/main/java/com/springjpa/repo/ServiceMasterRepository.java
+++ src/main/java/com/springjpa/repo/ServiceMasterRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.springjpa.model.ServiceMaster;
 
 public interface ServiceMasterRepository  extends JpaRepository<ServiceMaster, Integer>{
-	
+	void sysout();
 	List<ServiceMaster> findByDeptCode(String deptCode);
 
 }
